532f6c29118b2892f982df75c41f1d6b601bd8be
Tina Kuzmenko <tina.kuzmenko@gmail.com>
2020-06-16 18:22:24 +0200

    Меняет App из функционального в классовый компонент

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,35 +1,93 @@
-import React from 'react';
+import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
 import {Switch, Route, BrowserRouter} from 'react-router-dom';
 import WelcomeScreen from '../welcome-screen/welcome-screen.jsx';
 import GenreQuestionsScreen from '../genre-questions-screen/genre-questions-screen.jsx';
 import ArtistQuestionsScreen from '../artist-questions-screen/artist-questions-screen.jsx';
+import {GameType} from '../../helpers/constants.js';
 
-const App = ({errorsCount, questions}) => {
-  return <BrowserRouter>
-    <Switch>
-      <Route exact path="/">
+export default class App extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      step: -1,
+    };
+  }
+
+  _renderGameScreen() {
+    const {errorsCount, questions} = this.props;
+    const {step} = this.state;
+
+    const question = questions[step];
+
+    if (step === -1 || step >= questions.length) {
+      return (
         <WelcomeScreen
           errorsCount={errorsCount}
-          onWelcomeButtonClick={() => {}} />
-      </Route>
-      <Route exact path="/dev-genre">
-        <GenreQuestionsScreen
-          question={questions[0]}
-          onAnswer={() => {}} />
-      </Route>
-      <Route exact path="/dev-artist">
-        <ArtistQuestionsScreen
-          question={questions[1]}
-          onAnswer={() => {}} />
-      </Route>
-    </Switch>
-  </BrowserRouter>;
-};
+          onWelcomeButtonClick={() => {
+            this.setState({
+              step: 0,
+            });
+          }}
+        />
+      );
+    }
+
+    if (question) {
+      switch (question.type) {
+        case GameType.ARTIST:
+          return (
+            <ArtistQuestionsScreen
+              question={question}
+              onAnswer={() => {
+                this.setState((prevState) => ({
+                  step: prevState.step + 1,
+                }));
+              }}
+            />
+          );
+        case GameType.GENRE:
+          return (
+            <GenreQuestionsScreen
+              question={question}
+              onAnswer={() => {
+                this.setState((prevState) => ({
+                  step: prevState.step + 1,
+                }));
+              }}
+            />
+          );
+      }
+    }
+
+    return null;
+  }
+
+  render() {
+    const {questions} = this.props;
+
+    return <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          {this._renderGameScreen()}
+        </Route>
+        <Route exact path="/dev-genre">
+          <GenreQuestionsScreen
+            question={questions[0]}
+            onAnswer={() => {}} />
+        </Route>
+        <Route exact path="/dev-artist">
+          <ArtistQuestionsScreen
+            question={questions[1]}
+            onAnswer={() => {}} />
+        </Route>
+      </Switch>
+    </BrowserRouter>;
+  }
+}
 
 App.propTypes = {
   errorsCount: PropTypes.number.isRequired,
   questions: PropTypes.array.isRequired,
 };
-
-export default App;
