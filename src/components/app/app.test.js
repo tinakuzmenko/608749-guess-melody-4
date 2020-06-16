bfcc932fcd02034ca4bface6973bb71e741f1c08
Tina Kuzmenko <tina.kuzmenko@gmail.com>
2020-06-16 20:00:16 +0200

    Исправляет пути к аватаркам в тестовых моках

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -27,13 +27,13 @@ const questions = [
       src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
     },
     answers: [{
-      picture: `https://api.adorable.io/avatars/128/${Math.random()}`,
+      picture: `https://api.adorable.io/avatars/128/1`,
       artist: `John Snow`,
     }, {
-      picture: `https://api.adorable.io/avatars/128/${Math.random()}`,
+      picture: `https://api.adorable.io/avatars/128/2`,
       artist: `Jack Daniels`,
     }, {
-      picture: `https://api.adorable.io/avatars/128/${Math.random()}`,
+      picture: `https://api.adorable.io/avatars/128/3`,
       artist: `Jim Beam`,
     }],
   }
