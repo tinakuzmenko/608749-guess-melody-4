9c2bb5b512a9ea5d9a98a9a38a14be1a96421c83
Tina Kuzmenko <tina.kuzmenko@gmail.com>
2020-06-26 18:02:23 +0200

    Меняет src в моках

--- src/mocks/questions.js
+++ src/mocks/questions.js
@@ -8,13 +8,13 @@ export default [
       src: `https://upload.wikimedia.org/wikipedia/commons/4/47/Beethoven_Moonlight_2nd_movement.ogg`,
       genre: `rock`,
     }, {
-      src: `http://aux.incompetech.com/music/royalty-free/mp3-royaltyfree/Egmont%20Overture%20Finale.mp3`,
+      src: `https://upload.wikimedia.org/wikipedia/commons/9/9f/Carmen_-_Prelude_to_Act_1.ogg`,
       genre: `blues`,
     }, {
       src: `https://upload.wikimedia.org/wikipedia/commons/4/43/JOHN_MICHEL_CELLO-J_S_BACH_CELLO_SUITE_1_in_G_Prelude.ogg`,
       genre: `jazz`,
     }, {
-      src: `http://aux.incompetech.com/music/royalty-free/mp3-royaltyfree/Danse%20Macabre%20-%20Busy%20Strings.mp3`,
+      src: `https://upload.wikimedia.org/wikipedia/commons/d/de/W._A._Mozart_-_Die_Zauberfl%C3%B6te_-_18._Der_H%C3%B6lle_Rache_kocht_in_meinem_Herzen_%28Ferenc_Fricsay%2C_1953%29.ogg`,
       genre: `rock`,
     }],
   },
