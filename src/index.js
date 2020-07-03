1609e7e4cd53868b3a770fb27dbaecc75c00bf10
Tina Kuzmenko <tina.kuzmenko@gmail.com>
2020-07-03 18:26:00 +0200

    Подключает Redux, добавляет reducer

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from "redux";
+import {Provider} from "react-redux";
 import App from './components/app/app.jsx';
 import questions from './mocks/questions.js';
+import {reducer} from "./reducer.js";
 
 const Settings = {
   ERRORS_COUNT: 3
 };
 
+const store = createStore(reducer);
 const root = document.querySelector(`#root`);
 
 ReactDOM.render(
-    <App
-      errorsCount={Settings.ERRORS_COUNT}
-      questions={questions} />,
+    <Provider store={store}>
+      <App
+        errorsCount={Settings.ERRORS_COUNT}
+        questions={questions} />
+    </Provider>,
     root
 );
