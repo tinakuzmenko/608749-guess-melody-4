e87fe81c8c6488d8040f54a9c1d70a6c02e66063
Tina Kuzmenko <tina.kuzmenko@gmail.com>
2020-07-12 18:44:36 +0200

    Добавляет логику для начала новой игры после окончания

--- src/hocs/with-audio/with-audio.e2e.test.js
+++ src/hocs/with-audio/with-audio.e2e.test.js
@@ -4,7 +4,9 @@ import {configure, mount} from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import withAudio from "./with-audio.jsx";
 
-configure({adapter: new Adapter()});
+configure({
+  adapter: new Adapter()
+});
 
 const Player = (props) => {
   const {onPlayButtonClick, children} = props;
@@ -24,22 +26,20 @@ Player.propTypes = {
   ]).isRequired,
 };
 
+
 it(`Checks that HOC's callback turn on audio (play)`, () => {
   const PlayerWrapped = withAudio(Player);
   const wrapper = mount(<PlayerWrapped
     isPlaying={false}
     onPlayButtonClick={() => {}}
-    src=""
+    src={``}
   />);
 
   window.HTMLMediaElement.prototype.play = () => {};
-
   const {_audioRef} = wrapper.instance();
-
   jest.spyOn(_audioRef.current, `play`);
 
   wrapper.instance().componentDidMount();
-
   wrapper.find(`button`).simulate(`click`);
 
   expect(_audioRef.current.play).toHaveBeenCalledTimes(1);
@@ -50,17 +50,15 @@ it(`Checks that HOC's callback turn off audio (pause)`, () => {
   const wrapper = mount(<PlayerWrapped
     isPlaying={true}
     onPlayButtonClick={() => {}}
-    src=""
+    src={``}
   />);
 
   window.HTMLMediaElement.prototype.pause = () => {};
-
   const {_audioRef} = wrapper.instance();
 
   jest.spyOn(_audioRef.current, `pause`);
 
   wrapper.instance().componentDidMount();
-
   wrapper.find(`button`).simulate(`click`);
 
   expect(_audioRef.current.pause).toHaveBeenCalledTimes(1);
